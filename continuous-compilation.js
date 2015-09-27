540df35f1768d7fc2af30bc2126f469b78583dc6
Samuel Gélineau <gelisam+github@gmail.com>
2015-09-27 09:51:26 -0400

    setup continuous compilation

--- continuous-compilation.js
+++ continuous-compilation.js
@@ -1,11 +1,20 @@
+var fs = require("fs");
+var browserify = require("browserify");
+var babelify = require("babelify");
+
 var watch = require('watch');
 var execSync = require('child_process').execSync;
 
 function compile() {
   execSync(
-    "clear; /Users/gelisam/working/js/pong/node_modules/.bin/flow || true",
+    "(clear; /Users/gelisam/working/js/pong/node_modules/.bin/flow; jsx --strip-types --harmony src build) || true",
     {"stdio": 'inherit'}
   );
+  browserify("./src/main.js", { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(fs.createWriteStream("build/main.js"));
 }
 
 watch.watchTree('src', compile)
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 /* @flow */
+"use strict";
+require("babelify/polyfill");
+var React = require("react");
 
-console.log("hello!");
-console.log(React);
+
+console.log(1);
+
+var x: number = 2;
+console.log(x);
+
+let y: number = 3;
+console.log(y);
+
+console.log(4);
+
 React.render(
   <h1>Hello, world!</h1>,
   document.getElementById('example')
