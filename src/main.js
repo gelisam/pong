fd092517f24bb2771a71d234660f0afb4d83170a
Samuel Gélineau <gelisam+github@gmail.com>
2015-09-28 03:10:43 -0400

    draw a circle

--- src/main.js
+++ src/main.js
@@ -6,14 +6,21 @@ var classNames = require("classnames");
 
 // Coordinates (0,0) and (100,100) are the top-left and bottom-right corners of
 // the largest square which fits into the viewport, centered in both directions.
+
 var Rectangle = React.createClass({
   propTypes: {
     x: React.PropTypes.number.isRequired,
     y: React.PropTypes.number.isRequired,
     width: React.PropTypes.number.isRequired,
     height: React.PropTypes.number.isRequired,
-    color: React.PropTypes.string.isRequired,
-    style: React.PropTypes.object
+    color: React.PropTypes.string,
+    style: React.PropTypes.object,
+  },
+  getDefaultProps: function() {
+    return {
+      color: 'black',
+      style: {},
+    };
   },
   render: function() {
     var { x, y, width, height, color, style, ...other } = this.props;
@@ -26,6 +33,35 @@ var Rectangle = React.createClass({
     style.backgroundColor = color;
     
     return <div style={style} {...other} />;
+  },
+});
+
+var Circle = React.createClass({
+  propTypes: {
+    x: React.PropTypes.number.isRequired,
+    y: React.PropTypes.number.isRequired,
+    radius: React.PropTypes.number.isRequired,
+    color: React.PropTypes.string,
+    style: React.PropTypes.object,
+  },
+  getDefaultProps: function() {
+    return {
+      color: 'black',
+      style: {},
+    };
+  },
+  render: function() {
+    var { x, y, radius, color, style, ...other } = this.props;
+    style = style || {};
+    style.position = 'absolute';
+    style.left = `calc(${x}vmin + 50vw - 50vmin)`;
+    style.top = `calc(${y}vmin + 50vh - 50vmin)`;
+    style.width = `${2*radius}vmin`;
+    style.height = `${2*radius}vmin`;
+    style.borderRadius = '50%';
+    style.backgroundColor = color;
+    
+    return <div style={style} {...other} />;
   }
 });
 
@@ -33,7 +69,7 @@ var Paddle = React.createClass({
   width: 2,
   height: 10,
   propTypes: {
-    x: React.PropTypes.number.isRequired
+    x: React.PropTypes.number.isRequired,
   },
   getInitialState: function() {
     return {atBottom: false};
@@ -46,7 +82,6 @@ var Paddle = React.createClass({
     return <Rectangle
       x={this.props.x - this.width/2} y={y}
       width={this.width} height={this.height}
-      color="black"
       style={{'transition': 'top 1s'}}
       onClick={this.handleClick}
     />;
@@ -58,6 +93,7 @@ React.render(
     <Rectangle x={0} y={0} width={100} height={100} color="lightgrey" />
     <Paddle x={3} />
     <Paddle x={97} />
+    <Circle x={50} y={50} radius={2} />
   </div>,
   document.getElementById('root')
 );
