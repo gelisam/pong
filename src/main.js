4b8983ac5ecc6dd2a29f439d70aa1c556b9d0b13
Samuel Gélineau <gelisam+github@gmail.com>
2015-09-28 00:09:23 -0400

    add background, define everything in terms of rectangles

--- src/main.js
+++ src/main.js
@@ -4,7 +4,34 @@ require("babelify/polyfill");
 var React = require("react");
 var classNames = require("classnames");
 
+// Coordinates (0,0) and (100,100) are the top-left and bottom-right corners of
+// the largest square which fits into the viewport, centered in both directions.
+var Rectangle = React.createClass({
+  propTypes: {
+    x: React.PropTypes.number.isRequired,
+    y: React.PropTypes.number.isRequired,
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    color: React.PropTypes.string.isRequired,
+    style: React.PropTypes.object
+  },
+  render: function() {
+    var { x, y, width, height, color, style, ...other } = this.props;
+    style = style || {};
+    style.position = 'absolute';
+    style.left = `calc(${x}vmin + 50vw - 50vmin)`;
+    style.top = `calc(${y}vmin + 50vh - 50vmin)`;
+    style.width = `${width}vmin`;
+    style.height = `${height}vmin`;
+    style.backgroundColor = color;
+    
+    return <div style={style} {...other} />;
+  }
+});
+
 var Paddle = React.createClass({
+  width: 2,
+  height: 10,
   propTypes: {
     x: React.PropTypes.number.isRequired
   },
@@ -15,21 +42,22 @@ var Paddle = React.createClass({
     this.setState({atBottom: !this.state.atBottom});
   },
   render: function() {
-    var posClass = this.state.atBottom ? 'bottom' : 'top';
-    return (
-      <div
-        className={classNames('paddle', posClass)}
-        style={{'left': `${this.props.x}vmin`}}
-        onClick={this.handleClick}
-      />
-    );
+    var y = this.state.atBottom ? 98 - this.height : 2;
+    return <Rectangle
+      x={this.props.x - this.width/2} y={y}
+      width={this.width} height={this.height}
+      color="black"
+      style={{'transition': 'top 1s'}}
+      onClick={this.handleClick}
+    />;
   }
 });
 
 React.render(
   <div>
-    <Paddle x={2} />
-    <Paddle x={100} />
+    <Rectangle x={0} y={0} width={100} height={100} color="lightgrey" />
+    <Paddle x={3} />
+    <Paddle x={97} />
   </div>,
   document.getElementById('root')
 );
